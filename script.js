e9841593450461ecb2344624c664bd45ef333564
ArliumDev <arliumdev@gmail.com>
2025-01-08 11:03:19 +0000

    functionality complete. added step-by-step doc. non-styled yet

--- script.js
+++ script.js
@@ -1,57 +1,47 @@
-// Modal handling
-
+// Modal Handling
 const dialog = document.querySelector('dialog');
 const newBook = document.querySelector('.new-book');
 const addBook = document.querySelector('.add-book');
 const cancel = document.querySelector('.close-modal');
 
 newBook.addEventListener('click', () => dialog.showModal());
-
 addBook.addEventListener('click', (event) => {
   addBookToLibrary();
   dialog.close();
   event.preventDefault();
 });
-
 cancel.addEventListener('click', (event) => {
   dialog.close();
   event.preventDefault();
 });
 
-// 'Book' object constructor
-
+// Book Constructor and Prototype
 function Book(author, title, pages, read) {
   this.author = author;
   this.title = title;
   this.pages = pages;
-  this.read = read;
+  this.read = read === 'yes'; // Convert 'yes'/'no' string to boolean
 }
 
-// Books storaging
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
 
+// Book Storage
 const myLibrary = [];
 
 function addBookToLibrary() {
-  const bookData = bookDataFetch();
-  const newBook = bookEntry(bookData);
+  const bookData = fetchBookData();
+  const newBook = createBookEntry(bookData);
   displayBook(newBook);
 }
 
-// New Book object creation with the user form data. Necessary to return bookData to allow other functions to access the latest data
-
-function bookEntry(fetchedData) {
-  const newBook = new Book(fetchedData.author, fetchedData.title, fetchedData.pages, fetchedData.read);
-  myLibrary.push(newBook);
-  console.log(myLibrary);
-  return newBook;
-}
-
-// Retrieve data from user's input. Necessary to return bookData to allow other functions to access the latest data
-
-function bookDataFetch() {
+// Retrieve book data from form
+function fetchBookData() {
   const form = document.querySelector('form');
   const formData = new FormData(form);
   const bookData = {};
+
   for (const [key, value] of formData.entries()) {
     bookData[key] = value;
   }
@@ -61,32 +51,55 @@ function bookDataFetch() {
   return bookData;
 }
 
-// Card display creator for new books with remove button to delete from both display and array
+// Create new Book object and add it to myLibrary
+function createBookEntry(fetchedData) {
+  const newBook = new Book(fetchedData.author, fetchedData.title, fetchedData.pages, fetchedData.read);
+  myLibrary.push(newBook);
+  return newBook;
+}
 
+// Display Book on screen
 function displayBook(book) {
   const cardContainer = document.querySelector('.card-container');
   const card = document.createElement('div');
   card.classList.add('card');
 
+  const bookIndex = myLibrary.indexOf(book);
+
   card.innerHTML = `
     <h3>${book.title}</h3>
     <p>Author: ${book.author}</p>
     <p>Pages: ${book.pages}</p>
-    <p>Read: ${book.read}</p>
-    <button class="remove-book">Remove</button>
+    <p>Read: ${book.read ? 'Yes' : 'No'}</p>
+    <button class="toggle-read" data-index="${bookIndex}">${book.read ? 'Unread' : 'Read'}</button>
+    <button class="remove-book" data-index="${bookIndex}">Remove</button>
   `;
 
-  const removeButton = card.querySelector('.remove-book');
-  removeButton.addEventListener('click', () => {
-    const bookIndex = myLibrary.indexOf(book);
-    if (bookIndex > -1) {
-      myLibrary.splice(bookIndex, 1);
-    }
-    cardContainer.removeChild(card);
-    console.log(myLibrary);
-  });
+  // Add Event Listeners
+  card.querySelector('.toggle-read').addEventListener('click', () => toggleReadStatus(book, card));
+  card.querySelector('.remove-book').addEventListener('click', () => removeBook(book, card));
 
   cardContainer.appendChild(card);
 }
 
-/* If I don't use 'return bookData' and 'return newBook', either bookEntry() or displayBook() could know if any changes have been appeared. 'Return' specifies a value to be returned to the function caller. In these cases, bookEntry(bookData) and displayBook(newBook) */
+// Toggle Read Status
+function toggleReadStatus(book, card) {
+  book.toggleRead();
+  const toggleReadButton = card.querySelector('.toggle-read');
+
+  // Update the button text
+  toggleReadButton.textContent = book.read ? 'Unread' : 'Read';
+
+  // Update the Read status paragraph (the one before the button)
+  toggleReadButton.previousElementSibling.textContent = `Read: ${book.read ? 'Yes' : 'No'}`;
+}
+
+// Remove Book from Library and Display
+function removeBook(book, card) {
+  const bookIndex = myLibrary.indexOf(book);
+  if (bookIndex > -1) {
+    myLibrary.splice(bookIndex, 1);
+  }
+  card.remove();
+  console.log(myLibrary);
+}
